2c92db8268a1ff4c1c02618376ee74315fda87f3
Scott Westover <scottwestover2006@gmail.com>
2024-01-09 21:07:27 -0500

    updated game template to work with germs demo game
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,45 +1,20 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
-
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
-    }
-
-    preload ()
-    {
-        //  This is an example of a bundled image:
-        this.load.image('logo', logoImg);
-
-        //  This is an example of loading a static image from the public folder:
-        this.load.image('background', 'assets/bg.jpg');
-    }
-      
-    create ()
-    {
-        this.add.image(400, 300, 'background');
-
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
-}
+import Boot from './Boot.js';
+import Preloader from './Preloader.js';
+import MainMenu from './MainMenu.js';
+import MainGame from './Game.js';
 
 const config = {
     type: Phaser.AUTO,
-    parent: 'phaser-example',
     width: 800,
     height: 600,
-    scene: MyGame
+    backgroundColor: '#000000',
+    parent: 'phaser-example',
+    scene: [ Boot, Preloader, MainMenu, MainGame ],
+    physics: {
+        default: 'arcade',
+        arcade: { debug: false }
+    }
 };
 
-const game = new Phaser.Game(config);
+let game = new Phaser.Game(config);
